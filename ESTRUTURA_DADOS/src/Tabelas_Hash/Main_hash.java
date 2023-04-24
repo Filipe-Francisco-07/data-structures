948cd9cd15f7d02d4ce87e6fbbb7d37c89021be0
Filipe Francisco Franknberger <111152966+filipe-f-franknberger@users.noreply.github.com>
2023-04-24 09:39:52 -0300

    Add files via upload

---
+++ ESTRUTURA_DADOS/src/Tabelas_Hash/Main_hash.java
@@ -0,0 +1,21 @@
+package Tabelas_Hash;
+
+public class Main_hash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		hashTable HashTable = new hashTable(10);
+		
+		HashTable.inserir("123.123.123-12", "Herbert Richards");
+		HashTable.inserir("777.666.777-66", "Tuezin");
+		HashTable.inserir("413.753.156-32", "Sorriso Ronaldo");
+		
+		System.out.println(HashTable.recuperar("123.123.123-12"));
+		System.out.println(HashTable.recuperar("777.666.777-66"));
+		System.out.println(HashTable.recuperar("413.753.156-32"));
+
+		
+	}
+
+}
